f1a04d414c0875126397117d094bd64c095eb0a3
maraalexandra <mara.alexandra11@yahoo.com>
2020-03-02 10:37:25 +0200

    add classes, variables

--- src/main/java/org/fasttrackit/App.java
+++ src/main/java/org/fasttrackit/App.java
@@ -8,6 +8,35 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
-    }
-}
+        System.out.println( "Hello AnimalRescuer!" );
+
+        Animal animalReference = new Animal();
+        animalReference.name = "Izzy";
+        animalReference.age = 4;
+        animalReference.health = 10;
+        animalReference.hungerness = 1-10;
+        animalReference.mood = 1-10;
+        animalReference.favouriteGame = "Toy";
+
+        Rescuer rescuerReference = new Rescuer();
+        rescuerReference.name = "Mara";
+        rescuerReference.budget = 100;
+
+        Food foodReference = new Food();
+        foodReference.name = "Advance";
+        foodReference.price = 50;
+        foodReference.expirationDate = 2022;
+        foodReference.disponibility = true;
+
+        Activity activityReference = new Activity();
+        activityReference.name = "Running";
+
+        Vet vetReference = new Vet();
+        vetReference.name = "Jack";
+        vetReference.degree= "Veterinary";
+
+
+
+
+
+    } }
